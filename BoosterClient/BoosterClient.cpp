aae2f436421bef25af4bd31ffb552bf671d80d2f
SanjayPanchal <psanjay679@gmail.com>
2024-04-26 15:41:12 +0530

    adding only cpp files

--- BoosterClient/BoosterClient.cpp
+++ BoosterClient/BoosterClient.cpp
@@ -9,7 +9,35 @@ typedef struct _ThreadData {
 }ThreadData, *PThreadData;
 
 
-int main()
+int main(int argc, char *argv[])
 {
+
+    if (argc < 3) {
+        printf("Usage: <%s> <thread_id> <priority>", argv[0]);
+        exit(0);
+    }
+
+    int tid = atoi(argv[1]);
+    int priority = atoi(argv[2]);
+
+    HANDLE hFile;
+    ThreadData tdata;
+
+    hFile = CreateFile(L"\\\\.\\Booster", GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("error opening file. Error: (0x%08x)\n", GetLastError());
+        exit(-1);
+    }
+
+    tdata.Priority = priority;
+    tdata.ThreadId = tid;
+
+    if (!WriteFile(hFile, &tdata, sizeof(tdata), NULL, nullptr)) {
+        printf("Priority change failed. Error (0x%08x)\n", GetLastError());
+        return -1;
+    }
+
+    CloseHandle(hFile);
+
     return 0;
 }
--- ch4/Source.cpp
+++ ch4/Source.cpp
@@ -1,21 +1,24 @@
+#include "ntifs.h"
 #include "ntddk.h"
+
+typedef struct _ThreadData {
+	ULONG ThreadId;
+	int Priority;
+} ThreadData, *PThreadData;
 
 VOID DriverUnload(PDRIVER_OBJECT DriverObject) {
 	
 	UNREFERENCED_PARAMETER(DriverObject);
 	KdPrint(("Driver Unload Successful"));
 
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Booster");
+
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
 }
 
 NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
 NTSTATUS BoosterWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp);
-
-//BOOL WriteFile(
-//	_In_ HANDLE hFile,
-//	_In_reads_bytes_opt_(nNumberOfBytesToWrite) LPCVOID lpBuffer,
-//	_In_ DWORD nNumberOfBytesToWrite,
-//	_Out_opt_ LPDWORD lpNumberOfBytesWritten,
-//	_Inout_opt_ LPOVERLAPPED lpOverlapped);
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 
@@ -23,19 +26,85 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Reg
 
 	KdPrint(("Driver Load Successful"));
 	
+	NTSTATUS status = STATUS_SUCCESS;
+
 	DriverObject->DriverUnload = DriverUnload;
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = BoosterCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = BoosterCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_WRITE] = BoosterWrite;
 
-	return STATUS_SUCCESS;
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Booster"), symLink;
+	PDEVICE_OBJECT DeviceObject;
+
+	status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create Device Object: (0x%08x)", status));
+		return status;
+	}
+
+	symLink = RTL_CONSTANT_STRING(L"\\??\\Booster");
+
+	status = IoCreateSymbolicLink(&symLink, &devName);
+
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create symlink: (0x%08x)\n", status));
+		return status;
+	}
+
+	DriverObject->DeviceObject = DeviceObject;
+
+	return status;
 
 }
 
 NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
 	return STATUS_SUCCESS;
 }
 
 NTSTATUS BoosterWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
-	return STATUS_SUCCESS;
+
+	auto status = STATUS_SUCCESS;
+	ULONG_PTR information = 0;
+	PETHREAD Thread;
+
+	auto irpSp = IoGetCurrentIrpStackLocation(Irp);
+
+	do {
+
+		if (irpSp->Parameters.Write.Length < sizeof(ThreadData)) {
+			status = STATUS_BUFFER_TOO_SMALL;
+			break;
+		}
+
+		auto data = static_cast<ThreadData*>(Irp->UserBuffer);
+
+		if (data == nullptr || data->Priority < 1 || data->Priority > 31) {
+			status = STATUS_INVALID_PARAMETER;
+			break;
+		}
+
+		status = PsLookupThreadByThreadId(ULongToHandle(data->ThreadId), &Thread);
+
+		if (!NT_SUCCESS(status)) {
+			break;
+		}
+
+		auto oldPriority = KeSetPriorityThread(Thread, data->Priority);
+		KdPrint(("Change of priority for thread: %u from: %u to %u done successfully\n", data->ThreadId, oldPriority, data->Priority));
+
+		ObDereferenceObject(Thread);
+		information = sizeof(data);
+		Irp->IoStatus.Status = status;
+		Irp->IoStatus.Information = information;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	} while (false);
+
+	return status;
 }
